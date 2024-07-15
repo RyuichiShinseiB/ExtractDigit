dfa700e69c2a6e015c0ea6ebc10dfddcfbc72c3a
Hakushine <hakushine.ryu.here@gmail.com>
2024-07-15 15:15:44 +0900

    Corrected to an easy-to-understand mold type.

--- extract_digit/processing.py
+++ extract_digit/processing.py
@@ -6,38 +6,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.figure import Figure
 from matplotlib.patches import Polygon
-from pydantic import BaseModel
 
-
-class BoundingBox(BaseModel):
-    left: int
-    right: int
-    top: int
-    bottom: int
-
-    @staticmethod
-    def _get_bounding_box(img: cv2t.MatLike) -> "BoundingBox":
-        nonzero = np.argwhere(img != 0)
-        top, left = np.min(nonzero, 0)
-        bottom, right = np.max(nonzero, 0)
-        return BoundingBox(left=left, right=right, top=top, bottom=bottom)
+from .param_config import BoundingBox, QuadrilateralVertices, RangeTuple
 
 
 def crop_transform_show_digits(
     src: cv2t.MatLike,
-    pnts: tuple[tuple[int, int], ...],
-    dstsize: tuple[int, int],
+    # crop_area_vertices: tuple[tuple[int, int], ...],
+    crop_area_vertices: QuadrilateralVertices,
+    dst_size: tuple[int, int],
     *,
     imshow: bool = False,
-    roi: tuple[int, int, int, int] | None = None,
+    roi: BoundingBox | None = None,
     close_up_area: tuple[tuple[int, int], tuple[int, int]] | None = None,
 ) -> cv2t.MatLike:
     """Crops and rectifies a target area of an image to a rectangle
 
     Args:
         src (cv2t.MatLike): A source image.
-        pnts (tuple[tuple[int, int], ...]): Each vertex of the area to be cropped. The order is clockwise.
-        dstsize (tuple[int, int]): Image size after rectangle correction.
+        crop_area_vertices (tuple[tuple[int, int], ...]): Each vertex of the area to be cropped. The order is clockwise.
+        dst_size (tuple[int, int]): Image size after rectangle correction.
         imshow (bool, optional): Whether to compare images before and after conversion.  Defaults to False.
         roi (tuple[int, int, int, int] | None, optional): _description_. Defaults to None.
         close_up_area (tuple[tuple[int, int], tuple[int, int]] | None, optional): _description_. Defaults to None.
@@ -45,13 +33,13 @@ def crop_transform_show_digits(
     Returns:
         cv2t.MatLike: Cropped and corrected image.
     """  # noqa: E501
-    pts1 = np.array(pnts, np.float32)
-    height, width = dstsize
-    pts2 = np.array(
+    src_pts1 = np.array(crop_area_vertices.align_vertices(), np.float32)
+    height, width = dst_size
+    dst_pts2 = np.array(
         [[0, 0], [width, 0], [width, height], [0, height]], np.float32
     )
 
-    trans_mat = cv2.getPerspectiveTransform(pts1, pts2)
+    trans_mat = cv2.getPerspectiveTransform(src_pts1, dst_pts2)
     dst = cv2.warpPerspective(src, trans_mat, (width, height))
 
     if imshow:
@@ -61,7 +49,7 @@ def crop_transform_show_digits(
             xmin, ymin = 0, 0
             ymax, xmax = src.shape
         else:
-            xmin, xmax, ymin, ymax = roi
+            xmin, xmax, ymin, ymax = roi.unpack()
         ax = fig.add_subplot(1, 2, 1)
         ax.imshow(
             # src[ymin : ymax + 1, xmin : xmax + 1],
@@ -69,7 +57,14 @@ def crop_transform_show_digits(
             cmap=plt.get_cmap("Greys_r"),
             extent=(xmin - 0.5, xmax + 0.5, ymax + 0.5, ymin - 0.5),
         )
-        ax.add_patch(Polygon(pnts, fc="None", ec="red", alpha=0.5))
+        ax.add_patch(
+            Polygon(
+                crop_area_vertices.align_vertices(),
+                fc="None",
+                ec="red",
+                alpha=0.5,
+            )
+        )
         if close_up_area is not None:
             ax.set_xlim(close_up_area[0])
             ax.set_ylim(close_up_area[1][::-1])
@@ -138,7 +133,7 @@ def find_contours(
         drawing = cv2.cvtColor(base_img, cv2.COLOR_GRAY2RGB)
         for i, contour in enumerate(contours):
             x, y, width, height = cv2.boundingRect(contour)
-            aspect = _calc_aspect(width, height)
+            aspect = _calc_aspect((width, height))
             cv2.rectangle(
                 drawing, (x, y), (x + width, y + height), (0, 255, 0)
             )
@@ -161,16 +156,16 @@ def _is_over_area(contour: cv2t.MatLike, area: float) -> bool:
     return judge
 
 
-def _calc_aspect(width: int, height: int) -> float:
-    return max(width, height) / min(width, height)
+def _calc_aspect(aspect_range: tuple[float, float] | RangeTuple) -> float:
+    if isinstance(aspect_range, RangeTuple):
+        aspect_range = aspect_range.to_tuple()
+    return max(aspect_range) / min(aspect_range)
 
 
-def _is_inner_aspect(
-    contour: cv2t.MatLike, min_aspect: float, max_aspect: float
-) -> bool:
+def _is_inner_aspect(contour: cv2t.MatLike, aspect_range: RangeTuple) -> bool:
     _, _, w, h = cv2.boundingRect(contour)
-    aspect = _calc_aspect(w, h)
-    return aspect < max_aspect and aspect > min_aspect
+    aspect = _calc_aspect((w, h))
+    return aspect < aspect_range.maximum and aspect > aspect_range.minimum
 
 
 def _calc_bb_fill_ratio(contour: cv2t.MatLike) -> float:
@@ -195,16 +190,14 @@ def filtering_digit_contours(
     src_img: cv2t.MatLike,
     bb_filling_ratio: float = 0.3,
     bb_image_ratio: float = 3e-3,
-    inner_aspect_range: Sequence[float] = (1.3, 6),
+    inner_aspect_range: RangeTuple = RangeTuple(1.3, 6),
 ) -> Sequence[cv2t.MatLike]:
     extracted_contours = list(
         filter(
             lambda x: _is_longer_than_width(x)
             and _calc_bb_fill_ratio(x) > bb_filling_ratio
             and _calc_bb_img_ratio(x, src_img) > bb_image_ratio
-            and _is_inner_aspect(
-                x, inner_aspect_range[0], inner_aspect_range[1]
-            ),
+            and _is_inner_aspect(x, inner_aspect_range),
             contours,
         )
     )
@@ -232,7 +225,7 @@ def sort_digit_contours(
 
 
 def remove_image_margins(img: cv2t.MatLike) -> cv2t.MatLike:
-    bb = BoundingBox._get_bounding_box(img)
+    bb = BoundingBox.get_bounding_box(img)
     return img.copy()[bb.top : bb.bottom, bb.left : bb.right]
 
 
